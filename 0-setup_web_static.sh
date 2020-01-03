11a20e72705d22545666c2b67bc7093bc0f53ae9
Daniel E Rodriguez <dr2d4@live.com>
2020-01-03 18:13:37 -0500

    Install and config nginx

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Install and config nginx
+sudo apt update
+sudo apt -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "$ a \location /hbnb_static {\n\talias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
+sudo service nginx restart
