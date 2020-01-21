df12c9e6f4ff9d2640a516a44573422fa4265544
Daniel E Rodriguez <dr2d4@live.com>
2020-01-21 20:37:24 +0000

    task 2 test

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Creta server and new route to index
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Prints message Hello HBNB!'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Prints message HBNB'''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''Print passed text'''
+    spaced_text = text.replace('_', ' ')
+    return 'C {}'.format(spaced_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
