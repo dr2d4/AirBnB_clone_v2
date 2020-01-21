533f5d247db3506abe16c069606f58c8956cb821
Daniel E Rodriguez <dr2d4@live.com>
2020-01-21 20:27:42 +0000

    task 1 test

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+    Creta server and new route to index
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''Prints message Hello HBNB!'''
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    '''Prints message HBNB'''
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
