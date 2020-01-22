572b42d99cd5edf95694f8ee64460b5fb3109ad4
Daniel E Rodriguez <dr2d4@live.com>
2020-01-22 21:37:04 +0000

    [Fix] - pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def hello_hbnb():
     '''Prints message Hello HBNB!'''
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''Prints message HBNB'''
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
